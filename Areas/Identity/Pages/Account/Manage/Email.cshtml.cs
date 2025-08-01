089762adfb43947e48112b777e9e26a8e671fd3f
Ryan Groch <ryan.w.groch@gmail.com>
2025-08-01 09:53:33 -0500

    fix: email changes post-signup work correctly

--- Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -129,11 +129,11 @@ namespace LinkShortener.Areas.Identity.Pages.Account.Manage
                     $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                 StatusMessage = "Confirmation link to change email sent. Please check your email.";
-                return RedirectToPage();
+                return Redirect(callbackUrl);
             }
 
             StatusMessage = "Your email is unchanged.";
-            return RedirectToPage();
+            return RedirectToPage("Email");
         }
 
         public async Task<IActionResult> OnPostSendVerificationEmailAsync()
@@ -165,7 +165,7 @@ namespace LinkShortener.Areas.Identity.Pages.Account.Manage
                 $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
             StatusMessage = "Verification email sent. Please check your email.";
-            return RedirectToPage();
+            return RedirectToPage("Email");
         }
     }
 }
